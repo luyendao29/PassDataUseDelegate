687af7c79d29be60107e0173f55389fd39644323
Boss <luyenddgc00774@gmail.com>
2019-09-04 11:21:56 +0700

    da them project

---
+++ Delegate/Delegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Delegate
+//
+//  Created by Boss on 9/4/19.
+//  Copyright © 2019 LuyệnĐào. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
